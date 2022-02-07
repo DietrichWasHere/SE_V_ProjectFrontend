f073094177d97f1a8af8a0ce4c381bd440d2a4d9
Goed505s <eddie.gomez@eagles.oc.edu>
2022-02-07 14:07:02 -0600

    Added a sample profile page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,154 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!--<div id="app">-->
+  <v-app >
+    <v-card class="overflow-hidden">
+      <v-app-bar
+        absolute
+        color="#6A76AB"
+        dark
+        shrink-on-scroll
+        prominent
+        elevation="4"
+        fade-img-on-scroll
+        scroll-target="#scrolling-techniques-3"
+      >
+        <!--<template v-slot:img="{ props }">
+          <v-img
+            v-bind="props"
+            gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+          ></v-img>
+        </template>-->
+  
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+  
+   
+        <v-app-bar-title >Profile</v-app-bar-title>
+  
+        <v-spacer></v-spacer>    <v-spacer></v-spacer>
+  
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+  
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+
+
+
+  
+
+
+
+
+        <template v-slot:extension>
+          <v-tabs align-with-title>
+            <v-tab>Edit</v-tab>
+            <v-tab>Appointments</v-tab>
+            <v-tab>Messages</v-tab>
+          </v-tabs>
+        </template>
+      </v-app-bar>
+      <v-sheet
+        id="scrolling-techniques-3"
+        class="overflow-y-auto"
+        max-height="80%"
+      >
+        <v-container class = "description">
+
+          <br><br><br><br><br><br><br>
+          <br><br>
+          
+         <h>User: </h>
+         <br> <h>First Name: </h>
+         <br><h>Last Name: </h>
+         <br> <h>Email: </h>
+         <br> <h>Phone Number: </h>
+
+        </v-container>
+
+        
+   <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      bottom
+      temporary
+    
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Tab</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Tab</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Tab</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Tab</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+      </v-sheet>
+    </v-card>
+  </v-app>
+<!--</div>-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import Profile from './components/Profile';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+ 
+  },
+
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+   
+  watch: {
+      group () {
+        this.drawer = false
+      }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+  overflow: hidden !important;
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+}
+
+html::-webkit-scrollbar {
+  width: 0;
+  height: 0;
+}
+
+.description {
+  min-height: 1000px;
+  font-size: 20px;
+  font-display: verdana;
+  color: grey;
+  line-height: 26pt;
+ 
 }
 </style>

7e6bcd344923b97d4582509f5684e6e6f1458c0d
DietrichWasHere <Dietrich.Versaw@eagles.oc.edu>
2022-02-08 16:22:21 -0600

    we needed a views folder

---
+++ src/components/TestPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app>  
+      <v-card-title>
+        <h1>Joe Shmoe Goe Noe Oho!</h1>
+      </v-card-title>
+   </v-app>
+ 
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import {createRouter, createWebHistory} from 'vue-router'
-//import adminmenu from '../views/adminmenu.vue'
+import testpage from '../views/adminmenu.vue'
 //import studentMenu from '../views/studentMenu.vue'
 //import viewName from '../views/viewName.vue'
 

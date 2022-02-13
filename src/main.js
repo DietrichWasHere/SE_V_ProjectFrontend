6848277dd46959a1550d87bde69034e6023eedbd
DietrichWasHere <Dietrich.Versaw@eagles.oc.edu>
2022-02-13 17:16:31 -0600

    progress ?
    i need to npm install

--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 import App from './App.vue'
-import router from './router/index.js'
+import router from './router/index'
 
 // https://v3.router.vuejs.org/installation.html#direct-download-cdn
-
-
-// Vue.config.productionTip = false
-
 // createApp(App).use(router).mount('#app')
 
-/*new Vue({
+Vue.config.productionTip = false
+
+new Vue({
   router,
   render: h => h(App),
-}).$mount('#app')*/
+}).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
-import {createRouter, createWebHistory} from 'vue-router'
+//import {createRouter, createWebHistory} from 'vue-router'
+import Router from 'vue-router'
 import helloWorld from '../components/HelloWorld.vue'
 import helloWorld2 from '../components/HelloWorld2.vue'
 //import studentMenu from '../views/studentMenu.vue'
 //import viewName from '../views/viewName.vue'
 
-
+Vue.use(Router)
 
 // https://router.vuejs.org/guide/
 const routes = [
@@ -23,8 +24,13 @@ const routes = [
 
 // https://stackoverflow.com/questions/47591679/multiple-routers-with-vue
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+const router = new Router({
+  mode: 'history',
+  base:
+    process.env.NODE_ENV === 'development'
+      ? '/'
+      : '/webpages/studentapp_vue/dist/',
+  routes: routes
 })
+
 export default router

fed96123b6835a6e654d5fa528eb3bd676b81406
DietrichWasHere <Dietrich.Versaw@eagles.oc.edu>
2022-02-14 14:57:01 -0600

    router works
    router now works, will further fix

---
+++ src/router.js
@@ -0,0 +1,35 @@
+//import {createRouter, createWebHistory} from 'vue-router'
+import Vue from 'vue'
+import Router from 'vue-router'
+import helloWorld from './components/HelloWorld.vue'
+// import helloWorld2 from './components/HelloWorld2.vue'
+//import studentMenu from '../views/studentMenu.vue'
+//import viewName from '../views/viewName.vue'
+
+Vue.use(Router)
+
+// https://router.vuejs.org/guide/
+const routes = [
+  {
+    path: '/',
+    name: 'default',
+    component: helloWorld
+  },
+  {
+    path: '/hi',
+    name: 'hi',
+    component: helloWorld
+  }
+]
+
+
+// https://stackoverflow.com/questions/47591679/multiple-routers-with-vue
+
+export default new Router({
+  mode: 'history',
+  base:
+    process.env.NODE_ENV === 'development'
+      ? '/'
+      : '/',
+  routes: routes
+})

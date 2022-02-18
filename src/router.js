ad6bc09bb4bc0a74dbf313e685f98c65eebb7431
Goed505s <eddie.gomez@eagles.oc.edu>
2022-02-18 14:21:42 -0600

    Added a template for crud

--- src/router.js
+++ src/router.js
@@ -5,34 +5,46 @@ import Router from 'vue-router'
 //import helloWorld2 from './components/HelloWorld2.vue'
 //import studentMenu from '../views/studentMenu.vue'
 //import viewName from '../views/viewName.vue'
-import AdminCrudMenu from './views/AdminCrudMenu.vue'
+
+import Admin from './views/Admin.vue'
 import Profile from './views/Profile.vue'
 import LoginPage from './views/LoginPage.vue'
+import CreateAdmin from './views/CreateAdmin.vue'
+
 
 
 Vue.use(Router)
 
 // https://router.vuejs.org/guide/
 const routes = [
+
   {
     path: '/',
-    name: 'admincrudmenu',
-    component: AdminCrudMenu
-  },
-  {
-    path: '/profile',
     name: 'profile',
-    component: Profile
+    component: Profile,
+
+    
   },
   {
     path: '/loginpage',
     name: 'loginpage',
     component: LoginPage
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: Admin
+  },
+  {
+    path: '/createAdmin',
+    name: 'createadmin',
+    component: CreateAdmin
   }
 ]
 
 
 // https://stackoverflow.com/questions/47591679/multiple-routers-with-vue
+
 
 export default new Router({
   mode: 'history',

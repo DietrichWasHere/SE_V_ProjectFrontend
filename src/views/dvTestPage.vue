e571b4d9195cb2e8102f1b0bff77fca64bec89ac
DietrichWasHere <Dietrich.Versaw@eagles.oc.edu>
2022-03-07 14:46:37 -0600

    help
    push appt not working unauthorized

--- src/views/dvTestPage.vue
+++ src/views/dvTestPage.vue
@@ -1,35 +1,51 @@
 <template>
     <v-app>  
-      <h1>Joe Shmoe Goe Noe Oho!</h1>
-      <social-login/>
-      <p>{{this.orgs}}</p>
+      <p>{{this.appointments}}</p>
+      <v-btn color="primary" :loading="loading" @click.native="newApptTest">New Appt Test</v-btn>
    </v-app>
 </template>
 
 <script>
-import SocialLogin from '../components/SocialLogin.vue'
-import OrgServices from '@/services/OrgServices.js';
+import ApptServices from '@/services/AppointmentServices.js';
 
 export default {
-  components: { SocialLogin },
-  name: 'Profile',
+  name: 'Test',
   props: {
     msg: String
   },
   data() {
     return {
-      orgs: []
+      appointments: [],
+      apptEdit: null
     };
   },
   created() {
-    OrgServices.getOrgs()
+    console.log("test");
+    ApptServices.getAppointments()
     .then(response => {
-      this.orgs = response.data
+      this.appoinments = response.data
     })
     .catch(error => {
       console.log('There was an error:', error.response)
     })
   },
+  methods: {
+    newApptTest() {
+      console.log("test");
+      ApptServices.addAppointment({
+        tutorID: '1',
+        orgID: '1',
+        startDateTime: '2022-01-04 13:40',
+        endDateTime: '2022-01-04 13:45',
+        locationID: '1',
+        studentID: '1',
+        tutorRating: null,
+        tutorComments: null,
+        studentRating: null,
+        studentComments: null
+      });
+    },
+  }
 }
 </script>
 

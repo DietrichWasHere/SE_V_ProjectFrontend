eb03dc3d9152e9dfd8a57f953cb0fffe8a813b20
TimothyWhiteOC <47747244+TimothyWhiteOC@users.noreply.github.com>
2022-03-25 16:46:05 -0500

    Remove duplicate import

--- src/router.js
+++ src/router.js
@@ -20,7 +20,6 @@ import Profile from './views/ProfilePage.vue'
 import Inbox from './views/Inbox.vue'
 import ListUsers from './views/ListUsers.vue'
 import ListAdmin from './views/ListAdmin.vue'
-import Profile from './views/ProfilePage.vue'
 import CreateAdmin from './views/CreateAdmin.vue'
 
 import dvTestPage from './views/dvTestPage.vue'

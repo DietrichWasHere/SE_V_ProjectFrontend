606d6f6619712ae80ba9a77d24f6256df30414fa
Braden-Thompson <braden.thompson18@gmail.com>
2022-04-16 14:03:25 -0500

    x

--- src/views/ListTutors.vue
+++ src/views/ListTutors.vue
@@ -57,7 +57,7 @@
                   </v-col>
                   <v-col cols="12" sm="6" md="4">
                     <v-text-field
-                      v-model="editedItem.contract"
+                      v-model="editedItem.dateAgreementSigned"
                       label="Phone number"
                     ></v-text-field>
                   </v-col>
